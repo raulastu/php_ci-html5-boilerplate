94b3acba7e6a1e0d6b6a4fabdc5cda08870b05e9
raulooo <erreauele@gmail.com>
2013-05-16 15:21:12 -0500

    adding .access

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -2,7 +2,7 @@
 
 class Pages extends CI_Controller {
 
-	public function view($page = 'smartx_home')
+	public function view($page = 'home')
 	{
 		if (file_exists('application/views/pages/'.$page.'.php')){
 			// Whoops, we don't have a page for that!
